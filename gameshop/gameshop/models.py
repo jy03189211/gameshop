c069419f1d8a82d04f7b9e06d7feda3277afed23
Eppu Halmesaari <ehalmesa@niksula.hut.fi>
2017-01-12 02:00:45 +0200

    finished models, added test file

--- gameshop/gameshop/models.py
+++ gameshop/gameshop/models.py
@@ -1,22 +1,26 @@
 from django.db import models
+from django.utils import timezone
+import json
+#from django.contrib.postgres.fields import ArrayField
 
 #User can have developed games and purchased games
 class User(models.Model):
-    createdAt = models.DateTimeField(auto_now_add=True) #if error, probably needs default=timezone.now
-    updatedAt = models.DateTimeField(auto_now=True)
+    #createdAt = models.DateTimeField(default=timezone.now()) #if error, probably needs default=timezone.now
+    #updatedAt = models.DateTimeField(default='')
+    username = models.CharField(max_length=50,default='user_without_name')
     isDeveloper = models.BooleanField()
 
+
 class Order(models.Model):
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    #createdAt = models.DateTimeField(default=timezone.now())
+    #updatedAt = models.DateTimeField(auto_now=True)
     #Order has purchased games
-    orderOfUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Purchase(models.Model):
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
-    purchaseOfOrder = models.ForeignKey(Order, on_delete=models.CASCADE)
+    #createdAt = models.DateTimeField(default=timezone.now())
+    #updatedAt = models.DateTimeField(auto_now=True)
 
     #Purchase can belong to order or those already purchased by user (relation straight to user)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
@@ -24,29 +28,41 @@ class Purchase(models.Model):
 
 
 class Game(models.Model):
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    #createdAt = models.DateTimeField(default=timezone.now())
+    #updatedAt = models.DateTimeField(auto_now=True)
     url = models.URLField(max_length=200)
     name = models.CharField(max_length=50)
     price = models.FloatField()
     available = models.BooleanField()
-    category = models.CharField(max_length=200)
+
+    #category = models.CharField(max_length=200)
+    categories = models.CharField(max_length=50, default='')
+
+
     #Game has a creator
     createdBy = models.ForeignKey(User,on_delete=models.CASCADE)
     #Game may belong to developed games (relation straight to user) and purchased games
     purchasedGame = models.ForeignKey(Purchase, on_delete=models.CASCADE)
-    developedGame = modesl.ForeingKey(User, on_delete=models.CASCADE)
+
+    def setCategories(self, x):
+        self.categories = json.dumps(x)
+
+    def getCategories(self):
+        print(self.categories)
+        return json.loads(self.categories)
 
 class Savegame(models.Model):
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    #createdAt = models.DateTimeField(default=timezone.now())
+    #updatedAt = models.DateTimeField(auto_now=True)
     data = models.CharField(max_length=200)
     #Savegame belongs to a certain game
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class Score(models.Model):
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    #createdAt = models.DateTimeField(default=timezone.now())
+    #updatedAt = models.DateTimeField(auto_now=True)
     score = models.PositiveIntegerField()
     #Score belongs to a certain game
-    gameScore = models.ForeignKey(Game, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
