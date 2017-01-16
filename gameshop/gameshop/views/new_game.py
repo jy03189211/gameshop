9ee5e3ffa4836645d3fb8e623d2ebeffba0b569e
Roope Palomaki <roope.palomaki@me.com>
2017-01-16 14:47:41 +0200

    Added new game form, updated form styling

--- gameshop/gameshop/views/new_game.py
+++ gameshop/gameshop/views/new_game.py
@@ -1,4 +1,16 @@
-from django.views.generic import TemplateView
+from django import forms
+from django.shortcuts import render
+from django.views.generic import View
 
-class NewGameView(TemplateView):
-    template_name = "new_game.html"
+class NewGameForm(forms.Form):
+    name = forms.CharField(label="Name", required=True)
+    url = forms.CharField(label="URL", required=True)
+    price = forms.DecimalField(label="Price", min_value=0.0, max_value=1000.0, decimal_places=1)
+    available = forms.BooleanField(label="Available for purchase")
+    categories = forms.CharField(label="Categories")
+
+class NewGameView(View):
+
+    def get(self, request):
+        form = NewGameForm()
+        return render(request, "new_game.html", {"form": form})
