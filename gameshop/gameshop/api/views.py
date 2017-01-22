85fc979559fed13630583bce846fbd8a229d5481
Eppu Halmesaari <ehalmesa@niksula.hut.fi>
2017-01-22 22:21:36 +0200

    game object json

--- gameshop/gameshop/api/views.py
+++ gameshop/gameshop/api/views.py
@@ -1,87 +1,106 @@
 from django.http import Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
+from django.http import JsonResponse
 import json
 
 from gameshop.models import *
 
 
-def user_score_json(request, user_id):
+def user_score(request, user_id):
 
     return HttpResponse('', content_type="application/json")
 
-def user_game_savegame_json(request, user_id, game_id, savegame_id):
+def user_game_savegame(request, user_id, game_id, savegame_id):
 
     return HttpResponse('', content_type="application/json")
 
-def user_savegame_json(request, user_id, game_id=None):
+def user_savegame_single_game(request, game_id):
 
     return HttpResponse('', content_type="application/json")
 
-def user_order_json(request, user_id, order_id=None):
+def user_savegames(request, user_id):
 
     return HttpResponse('', content_type="application/json")
 
-def user_sale_json(request, user_id, sale_id=None):
+def user_order(request, user_id, order_id=None):
 
     return HttpResponse('', content_type="application/json")
 
-def user_purchased_json(request, user_id, game_id=None):
+def user_sale(request, user_id, sale_id=None):
 
     return HttpResponse('', content_type="application/json")
 
-def user_inventory_json(request, user_id, game_id=None):
+def user_purchased(request, user_id, game_id=None):
 
     return HttpResponse('', content_type="application/json")
 
-def user_json(request, user_id=None):
+def user_inventory(request, user_id, game_id=None):
 
     return HttpResponse('', content_type="application/json")
 
-def game_score_json(request, game_id):
+def users(request, user_id=None):
 
     return HttpResponse('', content_type="application/json")
 
-def game_json(request, game_id=None):
+def user_single(request, user_id=None):
 
-    #data = Country.objects.all().filter(continent=continent)
-    # countries = {}
-    # for country in data:
-    #     countries[country.code] = country.name
-    # my_json_data = json.dumps(countries)
+    return HttpResponse('', content_type="application/json")
 
-    if request.method == 'GET':
-        get_game(request, game_id)
+def game_score(request, game_id):
 
-        # if "callback" in request.GET:
-        #     callback = request.GET["callback"]
-        #     a = callback + "(" + my_json_data + ")"
-        #     return HttpResponse(a, content_type="text/javascript")
+    return HttpResponse('', content_type="application/json")
 
-    # elif request.method == 'POST':
-    #     set_game(request, game_id)
-    #
-
-def get_game(request, game_id=None):
+def game_single(request, game_id):
 
     game = get_object_or_404(Game, pk=game_id)
+    data = {
+    "created_at": game.created_at,
+    "updated_at": game.updated_at,
+    "url": game.url,
+    "name": game.name,
+    "description": game.description,
+    "price": game.price,
+    "available": game.available,
+    "categories": game.categories,
+    "created_by": game.created_by.public_name,
+    #"owned_by": game.owned_by.all(),
+     }
+    return JsonResponse(data)
 
-    objects_filtered = Game.objects
+def games(request):
 
-    for key in request.GET:
-        objects_filtered = objects_filtered.filter(key)
+    if request.method == 'GET':
+        filters = Game.objects
 
-    game_list = list(objects_filtered)
+        created_by = request.GET.get('createdby')
+        available = request.GET.get('available')
+        name = request.GET.get('name')
+        priceMin = request.GET.get('pricemin')
+        priceMax = request.GET.get('pricemax')
+        categories = request.GET.get('categories')
+        year = request.GET.get('year')
+        month = request.GET.get('month')
+        day = request.GET.get('day')
 
-    json_data = json.dumps(game_list)
-    # createdBy = request.GET.get('createdby')
-    # priceMin = request.GET.get('pricemin')
-    # priceMax = request.GET.get('pricemax')
-    # available = request.GET.get('available')
-    # category = request.GET.get('category')
-    # year = request.GET.get('year')
-    # month = request.GET.get('month')
-    # day = request.GET.get('day')
-    # Game.objects.filter(createdBy).filter(available).filter(category).filter(year).filter(month).filter(day)
-    return HttpResponse(json_data, content_type="application/json")
-# def set_game(request, game_id=None):
+        if created_by != None:
+            filters = filters.filter(created_by = created_by)
+
+        if available != None:
+            filters = filters.filter(available = available)
+
+        if categories != None:
+            filters = filters.filter(categories = categories)
+
+        if year != None:
+            print('year')
+
+        data = filters.all()
+        games = {}
+        for game in data:
+            games[game.id] = game.name
+        #print(games)
+        return JsonResponse(games)
+
+    elif request.method == 'POST':
+        print("POST")
