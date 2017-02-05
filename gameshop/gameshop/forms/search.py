7921677a0258d1d8f2cd15155ba20186461046ff
Roope Palomaki <roope.palomaki@me.com>
2017-02-05 22:02:01 +0200

    Added search functionality

---
+++ gameshop/gameshop/forms/search.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class SearchForm(forms.Form):
+    name = forms.CharField(label="Name", required=False, max_length=50)
+    price_min = forms.DecimalField(label="Price min", required=False, min_value=0.0, max_value=1000.0, decimal_places=2)
+    price_max = forms.DecimalField(label="Price max", required=False, min_value=0.0, max_value=1000.0, decimal_places=2)
+    created_by = forms.CharField(label="Developer", required=False, max_length=50)
+
+    # TODO: add more fields
--- gameshop/gameshop/views/search.py
+++ gameshop/gameshop/views/search.py
@@ -1,4 +1,57 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views.generic import View
+from gameshop.models import Game
+from gameshop.forms.search import SearchForm
 
-class SearchView(TemplateView):
+
+class SearchView(View):
     template_name = "search.html"
+
+    def get(self, request):
+
+        # TODO: very similar to the API code,
+        # separate json and object api functions and then change this to use it
+        #-------
+
+        filters = Game.objects
+
+        created_by = request.GET.get('created_by')
+        available = request.GET.get('available')
+        name = request.GET.get('name')
+        price_min = request.GET.get('pricemin')
+        price_max = request.GET.get('pricemax')
+        categories = request.GET.get('categories')
+
+        # TODO: public or not? availability always or not
+        filters.filter(available=True)
+
+        # optional filters
+        if name != None:
+            filters = filters.filter(name__icontains=name)
+        if price_min != None:
+            filters = filters.filter(price__gte=price_min)
+        if price_max != None:
+            filters = filters.filter(price__lte=price_max)
+        if created_by != None:
+            filters = filters.filter(created_by__public_name__icontains=created_by)
+
+
+        # if available != None:
+        #     filters = filters.filter(available = available)
+        # if categories != None:
+        #     filters = filters.filter(categories = categories)
+        # if year != None:
+        #     print('year')
+
+        data = filters.all()
+
+        #-------
+
+        results = data
+
+        form = SearchForm(initial=request.GET.dict())
+
+        return render(request, self.template_name, {
+            "results": results,
+            "form": form
+        })
