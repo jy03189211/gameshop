02b471e5dbee770ec88b2d1c417daf337f300fe5
Roope Palomaki <roope.palomaki@me.com>
2017-02-05 20:30:27 +0200

    Allowed double decimal prices

--- gameshop/gameshop/forms/game.py
+++ gameshop/gameshop/forms/game.py
@@ -6,7 +6,7 @@ class NewGameForm(forms.Form):
     name = forms.CharField(label="Name", required=True, max_length=50)
     url = forms.CharField(label="URL", required=True, max_length=200)
     description = forms.CharField(label="Description", required=True, max_length=500, widget=forms.Textarea)
-    price = forms.DecimalField(label="Price", min_value=0.0, max_value=1000.0, decimal_places=1)
+    price = forms.DecimalField(label="Price", min_value=0.0, max_value=1000.0, decimal_places=2)
     image = forms.FileField(label="Image")
     categories = forms.CharField(label="Categories")
     available = forms.BooleanField(label="Available for purchase")
