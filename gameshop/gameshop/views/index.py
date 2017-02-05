535b0bc21ffd24d469f490882728f796dfa8c449
Roope Palomaki <roope.palomaki@me.com>
2017-02-05 20:28:34 +0200

    Added featured games and jumbo to landing page, updated game screen layout

--- gameshop/gameshop/views/index.py
+++ gameshop/gameshop/views/index.py
@@ -1,4 +1,22 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views.generic import View
+from gameshop.models import Game
 
-class IndexView(TemplateView):
+class IndexView(View):
     template_name = "index.html"
+
+    def get_latest_games(self, n):
+        """Gets n most recently added games"""
+        return Game.objects.all().order_by('-created_by')[:n]
+
+    def get(self, request):
+        # 1 game in jumbo + 4 games featured
+        n = 5
+        latest_games = self.get_latest_games(n)
+        jumbo_game = latest_games[0]
+        featured_games = latest_games[1:n]
+
+        return render(request, self.template_name, {
+            "jumbo_game": jumbo_game,
+            "featured_games": featured_games
+        })
