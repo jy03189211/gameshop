228b1276bca4ebb4db9cc949b4b67d56d590bbf3
Roope Palomaki <roope.palomaki@me.com>
2017-02-10 21:53:14 +0200

    Fixed login and register form error display

--- gameshop/gameshop/forms/user.py
+++ gameshop/gameshop/forms/user.py
@@ -1,15 +1,18 @@
-from django import forms
 import re
+from django import forms
+from django.contrib.auth import authenticate
 from django.core.exceptions import ObjectDoesNotExist
-from ..models import User
-from django.forms import CharField, Form, PasswordInput
+from gameshop.models import User
+
 
 class RegisterForm(forms.ModelForm):
-    password=forms.CharField(label='password', widget=forms.PasswordInput(),)
-    password2 = forms.CharField(label='retype password', widget=forms.PasswordInput(),)
+    password = forms.CharField(label='password', widget=forms.PasswordInput(),)
+    password2 = forms.CharField(
+        label='retype password', widget=forms.PasswordInput(),)
+
     class Meta:
         model = User
-        fields=['username', 'email','password']
+        fields = ['username', 'email', 'password']
         write_only_fields = ['password']
 
     def clean_password2(self):
@@ -23,15 +26,18 @@ class RegisterForm(forms.ModelForm):
     def clean_username(self):
         username = self.cleaned_data['username']
         if not re.search(r'^\w+$', username):
-            raise forms.ValidationError('Username can only contain alphanumeric characters and the underscore.')
+            raise forms.ValidationError(
+                'Username can only contain alphanumeric characters and the underscore.')
         try:
             User.objects.get(username=username)
         except ObjectDoesNotExist:
             return username
         raise forms.ValidationError('Username is already taken.')
 
-class LoginForm(forms.ModelForm):
-    password = forms.CharField(label='password', widget=forms.PasswordInput(), )
-    class Meta:
-        model =User
-        fields=['username','password']
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        label='Username', max_length=30, widget=forms.TextInput())
+    password = forms.CharField(
+        label='Password', max_length=30, widget=forms.PasswordInput())
+
--- gameshop/gameshop/views/auth.py
+++ gameshop/gameshop/views/auth.py
@@ -1,18 +1,18 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.shortcuts import render
+from django.contrib.auth import views as auth_views, authenticate, login
+from django.shortcuts import render, redirect
 from gameshop.forms.user import RegisterForm, LoginForm
 
 
 def login_view_get(request):
     # get page for login and registration
     register_form = RegisterForm(request.GET)
-    login_form = LoginForm(request.GET) # TODO: use the django view instead of this
+    form = LoginForm(request.GET)
     return render(request, 'login.html', {
         'register_form': register_form,
-        'login_form': login_form}
-    )
+        'form': form
+    })
 
 
 def login_view(request):
@@ -23,7 +23,12 @@ def login_view(request):
 
     # if POST, redirect to the django built-in login view
     if request.method == 'POST':
-        return auth_views.login(request)
+        register_form = RegisterForm(request.GET)
+        return auth_views.login(
+            request, template_name='login.html', extra_context={
+                'register_form': register_form
+            }
+        )
 
 
 def register_view(request):
@@ -34,6 +39,27 @@ def register_view(request):
 
     # if POST, register a new user
     if request.method == 'POST':
-        # do register stuff
-        pass
+        register_form = RegisterForm(request.POST)
 
+        if register_form.is_valid():
+            user = register_form.save(commit=False)
+            username = register_form.cleaned_data['username']
+            password = register_form.cleaned_data['password']
+
+            # if the two password inputs match
+            register_form.clean_password2()
+            # if the username input is valid and unique
+            register_form.clean_username()
+            user.set_password(user.password)
+            user.save()
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('index')
+
+        # try again, something went wrong
+        return render(request, 'login.html', {
+            'register_form': register_form,
+            'form': LoginForm()
+        })
