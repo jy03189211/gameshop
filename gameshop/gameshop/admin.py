ba5f0b67714ac2cb3c4797f5b3a0517c04052ef6
Roope Palomaki <roope.palomaki@me.com>
2017-02-11 15:31:52 +0200

    Added admin panel support

---
+++ gameshop/gameshop/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import User, Game, Category, Order, Purchase, Savegame, Score
+
+admin.site.register(User)
+admin.site.register(Game)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Purchase)
+admin.site.register(Savegame)
+admin.site.register(Score)
