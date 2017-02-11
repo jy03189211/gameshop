4df91d2714e7d7a1555709d7560ab4691e9c188f
Roope Palomaki <roope.palomaki@me.com>
2017-02-12 00:45:40 +0200

    Fixed empty string bug in search request handling

--- gameshop/gameshop/views/search.py
+++ gameshop/gameshop/views/search.py
@@ -26,15 +26,17 @@ class SearchView(View):
         filters.filter(available=True)
 
         # optional filters
-        if name != None:
+        if name:
             filters = filters.filter(name__icontains=name)
         if price_min:
             filters = filters.filter(price__gte=price_min)
         if price_max:
             filters = filters.filter(price__lte=price_max)
-        if created_by != None:
+        if created_by:
             filters = filters.filter(created_by__public_name__icontains=created_by)
-        if category != None:
+
+        print(category)
+        if category:
             filters = filters.filter(category=category)
 
 
