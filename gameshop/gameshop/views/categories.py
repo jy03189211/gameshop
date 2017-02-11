9bf3b60633d6126707a12e9e3ab030b0950c97a9
Roope Palomaki <roope.palomaki@me.com>
2017-02-11 16:49:51 +0200

    Added functionality to categories page

--- gameshop/gameshop/views/categories.py
+++ gameshop/gameshop/views/categories.py
@@ -1,4 +1,28 @@
-from django.views.generic import TemplateView
+import string
+from django.shortcuts import render
+from django.views.generic import View
+from gameshop.models import Category
 
-class CategoriesView(TemplateView):
-    template_name = "categories.html"
+
+class CategoriesView(View):
+    """Shows all existing game categories"""
+
+    def get(self, request):
+        categories = Category.objects.all()
+
+        # build groups by first letters
+        category_groups = {}
+        for cat in categories:
+            # if key exists, append to list at it, else create and append
+            category_groups.setdefault(cat.name[0], []).append(cat)
+
+        first_letters = list(category_groups.keys())
+
+        # lowercase English alphabet
+        alphabet = list(string.ascii_lowercase)
+
+        return render(request, "categories.html", {
+            'category_groups': category_groups,
+            'first_letters': first_letters,
+            'alphabet': alphabet
+        })
