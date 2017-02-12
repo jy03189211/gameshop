b3032422ea6d299b0914b1371ef6b6ec6d5311b0
Roope Palomaki <roope.palomaki@me.com>
2017-02-12 21:48:18 +0200

    Moved helper out of the class

--- gameshop/gameshop/views/index.py
+++ gameshop/gameshop/views/index.py
@@ -2,21 +2,23 @@ from django.shortcuts import render
 from django.views.generic import View
 from gameshop.models import Game
 
-class IndexView(View):
-    template_name = "index.html"
 
-    def get_latest_games(self, n):
-        """Gets n most recently added games"""
-        return Game.objects.all().order_by('-created_by')[:n]
+def get_latest_games(n):
+    """Gets n most recently added games"""
+    return Game.objects.all().order_by('-created_at')[:n]
+
+
+class IndexView(View):
 
     def get(self, request):
         # 1 game in jumbo + 4 games featured
         n = 5
-        latest_games = self.get_latest_games(n)
+        latest_games = get_latest_games(n)
+
         jumbo_game = latest_games[0]
         featured_games = latest_games[1:n]
 
-        return render(request, self.template_name, {
+        return render(request, 'index.html', {
             "jumbo_game": jumbo_game,
             "featured_games": featured_games
         })
