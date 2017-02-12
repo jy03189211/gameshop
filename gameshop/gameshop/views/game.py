4ce461a31338d2bc2f1ba7adda52fb74bcbda21e
Roope Palomaki <roope.palomaki@me.com>
2017-02-12 16:43:55 +0200

    Added leaderboard and personal highscores

--- gameshop/gameshop/views/game.py
+++ gameshop/gameshop/views/game.py
@@ -1,12 +1,25 @@
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.utils.decorators import method_decorator
 from django.views.generic import View
-from django.shortcuts import render
-from gameshop.models import Game
+from django.shortcuts import render, get_object_or_404
+from gameshop.models import Game, Score
 
 class GameView(View):
 
     @method_decorator(ensure_csrf_cookie)
     def get(self, request, game_id):
-        game = Game.objects.get(pk=game_id)
-        return render(request, "game.html", { "game": game })
+
+        game = get_object_or_404(Game, pk=game_id)
+
+        # top 10 scores for the logged in user
+        your_highscores = Score.objects.filter(
+            game=game, user=request.user).order_by('-score')[:10]
+
+        # top 10 global leaderboard
+        leaderboard = Score.objects.filter(game=game).order_by('-score')[:10]
+
+        return render(request, 'game.html', {
+            'game': game,
+            'your_highscores': your_highscores,
+            'leaderboard': leaderboard
+        })
