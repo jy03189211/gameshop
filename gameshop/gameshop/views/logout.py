5d8580d64b308f05fea29028376de05d3f01dfd0
Roope Palomaki <roope.palomaki@me.com>
2017-02-12 22:17:57 +0200

    removed unused files

--- gameshop/gameshop/views/logout.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-from django.contrib.auth import logout
-from django.views.decorators.http import require_POST
-
-
-@require_POST
-def logout_view(request):
-    logout(request)
-    return redirect('/login/')
