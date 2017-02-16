633f33d7e91fb869b4a0d1fed5bed13c08939b62
Roope Palomaki <roope.palomaki@me.com>
2017-02-16 19:50:38 +0200

    Fixed game view, should not require login

--- gameshop/gameshop/views/game.py
+++ gameshop/gameshop/views/game.py
@@ -1,30 +1,30 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import View
 from gameshop.models import Game, Score
 
 
-@method_decorator(login_required, name='dispatch')
 class GameView(View):
 
     def get(self, request, game_id):
 
         game = get_object_or_404(Game, pk=game_id)
 
-        # top 10 scores for the logged in user
-        your_highscores = Score.objects.filter(
-            game=game, user=request.user).order_by('-score')[:10]
+        your_highscores = None
+        owned = False
+
+        if request.user.is_authenticated:
+            # top 10 scores for the logged in user
+            your_highscores = Score.objects.filter(
+                game=game, user=request.user).order_by('-score')[:10]
+
+            # if already owned
+            owned_games = request.user.owned_games.all()
+            if game in owned_games:
+                owned = True
 
         # top 10 global leaderboard
         leaderboard = Score.objects.filter(game=game).order_by('-score')[:10]
-
-        # already owned
-        owned = False
-        owned_games = request.user.owned_games.all()
-        if game in owned_games:
-            owned = True
 
         return render(request, 'game.html', {
             'game': game,
