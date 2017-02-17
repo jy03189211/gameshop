98d927925b28a7c735bc67199db88c9e73afc03e
Eppu Halmesaari <ehalmesa@niksula.hut.fi>
2017-02-17 16:13:40 +0200

    removed comment symbol

--- gameshop/gameshop/settings.py
+++ gameshop/gameshop/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
-    'livereload',
+    #'livereload',
     'gameshop.api',
 ]
 
@@ -52,11 +52,11 @@ MIDDLEWARE_CLASSES= [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-#    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
+    #'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'gameshop.urls'
